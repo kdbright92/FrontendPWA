59af1f5c6ff5daba1e4d5801f7d2d49175d77c4a
Kevin <kdbright92@gmail.com>
2024-02-23 16:32:12 +0100

    PWA Start

--- src/Components/Share/Share.jsx
+++ src/Components/Share/Share.jsx
@@ -221,7 +221,6 @@ export default function Share({ onShare }) {
                             onChange={handleFileChange}
                         />
 
-
                         <VideoCameraBack color="success" />
                         <PersonAdd color="error" onClick={() => setOpen(true)} />
 

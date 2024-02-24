82ea600512ec73fc971bcc8fb0ab0b32cf4e0757
Kevin <kdbright92@gmail.com>
2024-02-24 11:20:57 +0100

    sc

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,16 +5,20 @@ const urlsToCache = [
     '/',
     '/index.html',
     '/manifest.json',
-    '/App.js'
+
+
     // Add other URLs you want to cache
 ];
 
 self.addEventListener('install', (event) => {
+
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => {
                 return cache.addAll(urlsToCache);
+
             })
+
     );
 });
 
@@ -24,11 +28,47 @@ self.addEventListener('fetch', (event) => {
             .then((response) => {
                 // Cache hit - return the response from the cache
                 if (response) {
+                    // Update cache in the background
+                    fetchAndUpdateCache(event.request);
                     return response;
                 }
 
                 // Not in cache - fetch the resource from the network
-                return fetch(event.request);
+                return fetch(event.request)
+                    .then((response) => {
+                        // Clone the response to store it in the cache
+                        const clonedResponse = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                // Cache the fetched resource
+                                cache.put(event.request, clonedResponse);
+                            });
+
+                        return response;
+                    })
+                    .catch((error) => {
+                        console.error('Error fetching and caching:', error);
+                    });
             })
     );
 });
+
+
+async function fetchAndUpdateCache(request) {
+    try {
+        const response = await fetch(request);
+        if (response.status === 200) {
+            // Clone the response to store it in the cache
+            const clonedResponse = response.clone();
+
+            caches.open(CACHE_NAME)
+                .then((cache) => {
+                    // Update the cache with the new response
+                    cache.put(request, clonedResponse);
+                });
+        }
+    } catch (error) {
+        console.error('Error updating cache:', error);
+    }
+}
--- public/workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    globDirectory: 'build/',
-    globPatterns: ['**/*.{html,js,css,png,jpg,jpeg,gif,svg,json}'],
-    swDest: 'build/service-worker.js',
-    clientsClaim: true,
-    skipWaiting: true,
-};
